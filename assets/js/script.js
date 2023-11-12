f2d6368e2f011e0182df45a1a92a2fa3e3d8eda6
SteveDoesThings <supersteve729@gmail.com>
2023-11-11 23:16:25 -0700

    built skeleton of project

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+var questionsObj = {
+    questions: [
+        "Question 1",
+        "Question 2",
+        "Question 3",
+        "Question 4",
+        "Question 5",
+    ],
+    responsesQues1: [
+        "Answer 1",
+        "Answer 2",
+        "Answer 3",
+        "Answer 4"
+    ],
+    responsesQues2: [
+        "Answer 1",
+        "Answer 2",
+        "Answer 3",
+        "Answer 4"
+    ],
+    responsesQues3: [
+        "Answer 1",
+        "Answer 2",
+        "Answer 3",
+        "Answer 4"
+    ],
+    responsesQues4: [
+        "Answer 1",
+        "Answer 2",
+        "Answer 3",
+        "Answer 4"
+    ],
+    responsesQues5: [
+        "Answer 1",
+        "Answer 2",
+        "Answer 3",
+        "Answer 4"
+    ]
+}
+
+console.log(questionsObj.responsesQues2[2]);
